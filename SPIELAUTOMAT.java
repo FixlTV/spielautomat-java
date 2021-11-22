ec28106715bc3754384652f97f958857ba209176
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2021-11-22 22:42:56 +0100

    animation stuff

--- SPIELAUTOMAT.java
+++ SPIELAUTOMAT.java
@@ -9,20 +9,32 @@ public class Spielautomat
     private int z1, z2, z3;
     private Random zufall;
     private static int guthaben = 100;
+    private static int startcode = -1;
+    private static int highscore = 100;
+    private static int pointswon = 0;
+    private static int games_played = 0;
     
-    public Spielautomat()
+    public Spielautomat() throws IOException
     {
-        zufall = new Random();
+        main(null);
+        // zufall = new Random();
      
-        z1 = 7;
-        z2 = 7;
-        z3 = 7;
-        spielwalze1 = new SPIELWALZE(10, 10, 50, z3);
-        spielwalze2 = new SPIELWALZE(60, 10, 50, z2);
-        spielwalze3 = new SPIELWALZE(110, 10, 50, z1);
+        // z1 = 7;
+        // z2 = 7;
+        // z3 = 7;
+        // spielwalze1 = new SPIELWALZE(10, 10, 50, z3);
+        // spielwalze2 = new SPIELWALZE(60, 10, 50, z2);
+        // spielwalze3 = new SPIELWALZE(110, 10, 50, z1);
     }
 
-    public Spielautomat(int breite, int hoehe) {
+    public Spielautomat(int breite, int hoehe) throws IOException {
+        if(startcode == -1) {
+            System.out.println("BlueJ Startschutz ausgelöst.");
+            System.out.println("Zur Verwendung des Automaten bitte die Main Methode aufrufen.");
+            System.out.println("");
+            main(null);
+            return;
+        }
         int laenge = breite / 7;
 
         zufall = new Random();
@@ -35,21 +47,23 @@ public class Spielautomat
         spielwalze3 = new SPIELWALZE(breite / 2 + laenge / 2, 10, laenge, z1);
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        File file = new File("data/score.txt");
+    public static void main(String[] args) throws IOException  {
+        File file = new File("data/scores.txt");
         if(!file.exists()) {
-            Writer w = new FileWriter("data/score.txt");
-            w.write(String.valueOf(guthaben));
-            w.close();
+            speichern("data/scores.txt", String.valueOf(guthaben) + " " + String.valueOf(highscore) + " " + String.valueOf(pointswon) + " " + String.valueOf(games_played));
         }
-        Scanner score = new Scanner(file);
-        score.useDelimiter(" ");
-        guthaben = Integer.parseInt(score.next());
-        score.close();
+        Scanner filescanner = new Scanner(file);
+        filescanner.useDelimiter(" ");
+        guthaben = Integer.parseInt(filescanner.next());
+        highscore = Integer.parseInt(filescanner.next());
+        pointswon = Integer.parseInt(filescanner.next());
+        games_played = Integer.parseInt(filescanner.next());
+        filescanner.close();
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
         int width = (int)size.getWidth();
         int heigth = (int)size.getHeight();
         new ZEICHENFENSTER("test", width, heigth, true, true);
+        startcode = 1;
         Spielautomat spiel = new Spielautomat(width, heigth);
         spiel.zeichne();
         Scanner sc = new Scanner(System.in);
@@ -95,34 +109,55 @@ public class Spielautomat
         spielwalze3.zeichne();
     } 
     
-    public void aktualisiere(int z1Neu, int z2Neu, int z3Neu) throws InterruptedException
+    public void aktualisiere(int z1Neu, int z2Neu, int z3Neu)
     {
-        Random random = new Random();
-        spielwalze1.faerbeUm(z1);
-        spielwalze2.faerbeUm(z2);
-        spielwalze3.faerbeUm(z3);
+        for (int i = 0; i < 15; i++) {
+            z1++;
+            if(z1 == 9) z1 = 0;
+            spielwalze1.faerbeUm(z1);
+            z2++;
+            if(z2 == 9) z2 = 0;
+            spielwalze2.faerbeUm(z2);
+            z3++;
+            if(z3 == 9) z3 = 0;
+            spielwalze3.faerbeUm(z3);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {}
+        }
         while(z1 != z1Neu || z2 != z2Neu || z3 != z3Neu) {
             if(z1 != z1Neu) {
-                z1 = random.nextInt(9);
+                z1++;
+                if(z1 == 9) z1 = 0;
                 spielwalze1.faerbeUm(z1);
             }
             if(z2 != z2Neu) {
-                z2 = random.nextInt(9);
+                z2++;
+                if(z2 == 9) z2 = 0;
                 spielwalze2.faerbeUm(z2);
             }
-            if(z3 != z2Neu) {
-                z3 = random.nextInt(9);
+            if(z3 != z3Neu) {
+                z3++;
+                if(z3 == 9) z3 = 0;
                 spielwalze3.faerbeUm(z3);
             }
-            Thread.sleep(200);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {}
         }
     }
     
-    public void spiele() throws IOException, InterruptedException
+    public void spiele() throws IOException
     {
         aktualisiere(zufall.nextInt(9), zufall.nextInt(9), zufall.nextInt(9));
         guthaben += guthaben();
-        speichern("data/score.txt", String.valueOf(guthaben));
+        switch(guthaben()) {
+            case 9: pointswon += 7;
+            case 2: pointswon += 3;
+            case -1: games_played += 1;
+        }
+        if(guthaben > highscore) highscore = guthaben;
+        speichern("data/scores.txt", String.valueOf(guthaben) + " " + String.valueOf(highscore) + " " + String.valueOf(pointswon) + " " + String.valueOf(games_played));
     }
     
     public int guthaben()
@@ -134,7 +169,7 @@ public class Spielautomat
         } else return -1;
     }
     
-    public void speichern(String file, String value) throws IOException 
+    public static void speichern(String file, String value) throws IOException 
     {
         Writer w = new FileWriter(file);
         w.write(value);
